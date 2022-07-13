de71868710f91f471450189523178d80b42de900
WaveOfCoding <waveformwstcrz@gmail.com>
2022-07-13 16:36:31 +0300

    feature: Add filters, search, ordering

--- store/models.py
+++ store/models.py
@@ -4,4 +4,9 @@ from django.db import models
 class Book(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    author_name = models.CharField(max_length=255)
+
+
+    def __str__(self):
+        return f'ID {self.id} : {self.name}'
 
